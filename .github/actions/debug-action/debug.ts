0e102c509177f5bb6441a3b651a2fee5a64e5d0d
Risto Novik <est.risto@gmail.com>
2020-05-03 14:47:17 +0300

    fix: Add debug action

---
+++ .github/actions/debug-action/debug.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+const run = async (): Promise<void> => {
+  try {
+    const creature = core.getInput('amazing-creature')
+    if (creature === 'mosquito') {
+      core.setFailed('Sorry, mosquitos are not amazing 🚫🦟')
+      return
+    }
+    const pusherName = github.context.payload.pusher.name
+    const message = `👋 Hello ${pusherName}! You are an amazing ${creature}! 🙌`
+    core.debug(message)
+    core.setOutput('amazing-message', message)
+  } catch (error) {
+    core.setFailed(`Debug-action failure: ${error}`)
+  }
+}
+
+run();
+
+export default run;
